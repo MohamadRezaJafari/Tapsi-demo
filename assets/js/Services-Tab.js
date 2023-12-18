a015e664ff6977df8376b082a592ae3c0d2ec9d1
Mohammad Reza Jafari <dev.mohamadrezajafari@gmail.com>
2023-12-18 23:16:37 +0330

    initial commit

---
+++ assets/js/Services-Tab.js
@@ -0,0 +1,15 @@
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targeTabContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+
+    // REMOVE PREVIOUS ACTIVED CLASSES
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+
+    // ADD NEW ACTIVE CLASSES
+    tab.classList.add("active");
+    targeTabContent.classList.add("active");
+  });
+});
